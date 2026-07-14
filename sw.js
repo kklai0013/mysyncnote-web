4f505517c04db1034ba338c3f7946634ac1640b7
kklai0013 <95528806+kklai0013@users.noreply.github.com>
2026-07-14 17:55:42 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE='mysyncnote-v1';
+const FILES=['./','./index.html','./manifest.webmanifest','./icon.svg'];
+self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(FILES))));
+self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key))))));
+self.addEventListener('fetch',event=>event.respondWith(fetch(event.request).catch(()=>caches.match(event.request))));
